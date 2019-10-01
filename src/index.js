55d4e908757775703c7185193c715a85934c0cf0
LyssenkoAlex <lyssenko.alex@gmail.com>
2019-10-01 13:22:59 +0600

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+    let arrBrackets = [];
+
+    bracketsConfig.forEach(i => {
+        i.forEach(y => {
+            arrBrackets.push(y)
+        })
+    });
+
+    let arrSameBrackets = [];
+
+    arrBrackets.forEach((i, index) => {
+        if (arrBrackets[index - 1] === i) {
+            arrSameBrackets.push(i)
+        }
+    });
+
+    if (str.length % 2 !== 0) return false;
+    for (let i = 0; i < str.length; i++) {
+        if (arrSameBrackets.includes(str[i])) {
+            if (str[i] === stack[stack.length - 1]) {
+                stack.pop();
+            } else stack.push(str[i]);
+        } else if (arrBrackets.indexOf(str[i]) % 2 === 1) {
+            if (stack[stack.length - 1] === arrBrackets[arrBrackets.indexOf(str[i]) - 1]) {
+                stack.pop()
+            }
+        } else stack.push(str[i])
+    }
+    return stack.length === 0;
 }
